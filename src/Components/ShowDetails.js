3092dfea0be7117c2f99a8d8ee9ee5f90dcacd2b
bengigenc <genc.bengi@gmail.com>
2022-09-07 19:34:11 +0300

    Reverting to 44221952302034a98af2a9d7f159a279dde772d1

--- src/Components/ShowDetails.js
+++ src/Components/ShowDetails.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { fetchDetails } from '../Redux/showDetails';
 
 export default function ShowDetails() {
-  const details = useSelector((state) => state.details);
+  const details = useSelector(((state) => state.details));
   const dispatch = useDispatch();
   const { id } = useParams();
   useEffect(() => {
@@ -13,34 +13,7 @@ export default function ShowDetails() {
 
   return (
     <div>
-      <div className="show-navigation">
-        <Link to="/">
-
-          <p className="icon">
-            <span className="material-symbols-outlined">arrow_back_ios</span>
-          </p>
-        </Link>
-
-        <h3>{details.name}</h3>
-      </div>
-
-      <div className="show-header">
-        <img src={details.image.medium} alt="poster" />
-        <div className="show-heading">
-          <p>{details.rating.average}</p>
-          <p>
-            {details.runtime}
-            {' '}
-            min
-          </p>
-        </div>
-      </div>
-
-      <ul>
-        {details.genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
+      <h3>{details.name}</h3>
     </div>
   );
 }
